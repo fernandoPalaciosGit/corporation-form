e0cf5e73fb639ee70ddfec75b79ed71f6df5966c
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2015-07-25 22:35:52 +0200

    namespace pattern to APP context

--- app/front/formMemberCorporate.ctr.js
+++ app/front/formMemberCorporate.ctr.js
@@ -1,4 +1,4 @@
-;(function ($, w, d, IndexedDB, FormValidation, Materialize, widgetTeamMember) {
+;( function ($, w, d, IndexedDB, FormValidation, Materialize, widgetTeamMember) {
     'use strict';
         
     var APP = {
@@ -9,173 +9,191 @@
             NAME: 'corporation',
             DOCUMENT: 'teamMembers',
             VERSION: 1
+        },
+        selectors : ( function () {
+            var $widget = $('#widget-suscribe-member');
+            return {
+                $widgetSuscribeMember : $widget,
+                $undoModified : $widget.find('.js-submit-edit-cancel-member'),
+                $removeMember : $widget.find('.js-modal-remove-member'),
+                $orderMembers : $widget.find('.js-order-member-data'),
+                // FORM CONTROL ELEMENTS
+                $tableMembers : $widget.find('.js-table-members'),
+                $formMemberDni : $widget.find('.js-control-team-dni'),
+                $formMemberName : $widget.find('.js-control-team-name'),
+                $formMemberCharge : $widget.find('.js-control-team-charge:input'),
+                $formMemberBirthdate : $widget.find('.js-control-team-birthdate')
+            };
+        })(),
+        resetFormStatus : function (newState, arrOldState) {
+            this.widgetTeamMember.changeWidgetDomState(this.selectors.$widgetSuscribeMember, newState, arrOldState);
+            this.formWidget.reset();
+            this.selectors.$formMemberCharge.prop('selectedIndex', 0).material_select();
+            this.formWidget.changeInputStyleState('.js-control-form:input', null);
+        },
+        // initialize active database
+        openMemberDatabase : function () {
+            this.localDB.openIndexedDBDatabase(
+                    this.DB.NAME,
+                    this.DB.VERSION,
+                    this.widgetTeamMember.getDocumentData(this.DB.DOCUMENT)
+                )
+                .done($.proxy(this.loadMembersData, this))
+                .progress(this.notifyProggress);
+        },
+        // load all objects from document into widget
+        loadMembersData : function () {
+            this.localDB.loadIndexedDBData(this.DB.DOCUMENT, 'readonly')
+                .done($.proxy( function (data) {
+                    var hasRequestData = $.isArray(data) && data.length > 0;
+                    
+                    hasRequestData && this.widgetTeamMember.refreshTableWidget(this.selectors.$tableMembers, data);
+                    this.selectors.$tableMembers.toggleClass('hide', !hasRequestData);
+                }, this))
+                .fail( function () {
+                    Materialize.toast('Fail, Cannot connect your Database.', 3000, 'rounded');
+                })
+                .always($.proxy( function () {
+                    this.selectors.$undoModified.trigger('click');
+                }, this))
+                .progress(this.notifyProggress);
+        },
+        // insert new object into document
+        insertMemberData : function (optionsMember) {
+            this.localDB.insertIndexedDBData(this.DB.DOCUMENT, 'readwrite', optionsMember)
+                .done($.proxy( function () {
+                    this.loadMembersData();
+                    Materialize.toast('Added new Member.', 3000, 'rounded');
+                }, this))
+                .fail( function () {
+                    Materialize.toast('Fail, dni must be unique.', 3000, 'rounded');
+                })
+                .progress(this.notifyProggress);
+        },
+        editMemberData : function (optionsMember) {
+            this.localDB.updateIndexedDBData(this.DB.DOCUMENT, 'readwrite', optionsMember)
+                .done($.proxy( function () {
+                    this.loadMembersData();
+                    Materialize.toast('Updated Member.', 3000, 'rounded');
+                }, this))
+                .fail( function () {
+                    Materialize.toast('Fail, dni must be unique.', 3000, 'rounded');
+                })
+                .progress(this.notifyProggress);
+        },
+        // load data from one object of document into form widget
+        loadFormMemberData : function (indexObject) {
+            this.localDB.getIndexedDBData(this.DB.DOCUMENT, 'readonly', indexObject)
+                .done($.proxy( function (dataMember) {
+                    this.resetFormStatus('wrapper__edit-member', ['wrapper__insert-member']);
+                    this.formWidget.changeMemberData = dataMember;
+                    this.widgetTeamMember.fillDataForm(this.formWidget.$form, dataMember);
+                }, this))
+                .fail( function () {
+                    Materialize.toast('Fail, could not get data.', 3000, 'rounded');
+                })
+                .progress(this.notifyProggress);
+        },
+        removeMember : function (indexObject) {
+            this.localDB.removeIndexedDBData(this.DB.DOCUMENT, 'readwrite', indexObject)
+                .done($.proxy( function () {
+                    this.loadMembersData();
+                    Materialize.toast('Removed Member.', 3000, 'rounded');
+                }, this));
+        },
+        // validate form input before insert or update member
+        isCustomValidateForm : function () {
+            this.formWidget.setCustomValidationItems(null);
+            // Issue Materialize : https://github.com/Dogfalo/materialize/issues/1647
+            $('.lean-overlay').remove();
+            this.formWidget.changeInputStyleState('.js-control-form:input', true);
+            
+            // validate multiple input states
+            var isValidateForm =    this.formWidget.nativeValidate() &
+                                    this.formWidget.checkDni(this.selectors.$formMemberDni) &
+                                    this.formWidget.checkBirthDateUI(this.selectors.$formMemberBirthdate);
+            
+            !isValidateForm && this.formWidget.$modalWidget.openModal() ;
+            return isValidateForm;
+        },
+        // actual form values 
+        getOptionsMember : function () {
+            return {
+                dni: this.selectors.$formMemberDni.val(),
+                name: this.selectors.$formMemberName.val(),
+                charge: this.selectors.$formMemberCharge.find(':selected').text(),
+                birthdate: this.selectors.$formMemberBirthdate.val()
+            };            
+        },
+        notifyProggress : function (msg, type) {
+            switch(type || '') {
+                case 'info':
+                    console.info(msg);
+                    break;
+                case 'error':
+                    console.error(msg);
+                    break;
+                case 'warn':
+                    console.warn(msg);
+                    break;
+                default:
+                    console.log(msg);
+            }
+        },
+        initialize : function () {
+            /* CONTEXT : window.APP */
+            this.widgetTeamMember.initDomElements('.datepicker', '.js-control-team-charge');
+            this.formWidget.setFormMessages(this.widgetTeamMember.getMessageValidation());
+            
+            this.formWidget.$form
+                .on('submit', function (ev) {
+                    ev.preventDefault(); // prevent redirect
+                })
+                .on('click', '.js-submit-insert-member', $.proxy( function () {
+                    if (this.isCustomValidateForm()) {
+                        this.insertMemberData(this.getOptionsMember());    
+                    }
+                }, this))
+                .on('click', '.js-submit-edit-save-member', $.proxy( function () {
+                    var optionsMember = $.extend(true, {}, this.formWidget.changeMemberData, this.getOptionsMember()); 
+                    if (!$.isEmptyObject(this.formWidget.changeMemberData) && this.isCustomValidateForm()) {
+                        this.editMemberData(optionsMember);
+                    }
+                }, this))
+                .on('click', '.js-submit-edit-remove-member', $.proxy( function () {
+                    this.formWidget.setConfirmationRemove(this.formWidget.changeMemberData.name);
+                }, this));
+                
+            this.formWidget.$form.find(':input').on('invalid', function (ev) {
+                ev.preventDefault(); // prevent show mesage native validity
+            });
+            
+            this.selectors.$undoModified.on('click', $.proxy( function () {
+                this.formWidget.changeMemberData = null;
+                this.resetFormStatus('wrapper__insert-member', ['wrapper__edit-member']);
+                this.formWidget.$modalRemove.closeModal();
+            }, this));
+            
+            this.selectors.$removeMember.on('click', $.proxy( function () {
+                this.removeMember(this.formWidget.changeMemberData.primaryKey);
+            }, this));
+            
+            this.selectors.$orderMembers.on('click', $.proxy( function (ev) {
+                var dataField = ev.currentTarget.dataset.field,
+                    indexField = this.widgetTeamMember.getDocumentData(this.DB.DOCUMENT).fields[dataField][0];
+                console.log('Order index : ' + indexField);
+                // resetOrderStatus(ev.currentTarget);
+                // orderMemberData(indexField);
+            }, this));
+            
+            this.selectors.$tableMembers.find('tbody').on('click', 'tr', $.proxy( function (ev) {
+                var indexObject = ev.currentTarget.dataset.indexeddbIndex;
+                this.loadFormMemberData(indexObject);
+            }, this));
+            
+            this.openMemberDatabase();
         }
     };
-            
-    $(d).ready(function () {
-        var localDB = APP.localDB,
-            formWidget = APP.formWidget,
-            widgetTeamMember = APP.widgetTeamMember,
-            $widgetSuscribeMember = $('#widget-suscribe-member'),
-            $undoModified = $widgetSuscribeMember.find('.js-submit-edit-cancel-member'),
-            $removeMember = $widgetSuscribeMember.find('.js-modal-remove-member'),
-            // FORM CONTROL ELEMENTS
-            $tableMembers = $widgetSuscribeMember.find('.js-table-members'),
-            $formMemberDni = $widgetSuscribeMember.find('.js-control-team-dni'),
-            $formMemberName = $widgetSuscribeMember.find('.js-control-team-name'),
-            $formMemberCharge = $widgetSuscribeMember.find('.js-control-team-charge:input'),
-            $formMemberBirthdate = $widgetSuscribeMember.find('.js-control-team-birthdate'),
-            resetFormStatus = function (newState, arrOldState) {
-                widgetTeamMember.changeWidgetDomState($widgetSuscribeMember, newState,arrOldState);
-                formWidget.reset();
-                $formMemberCharge.prop('selectedIndex', 0).material_select();
-                formWidget.changeInputStyleState('.js-control-form:input', null);
-            },
-            // initialize active database
-            openMemberDatabase = function () {
-                localDB.openIndexedDBDatabase(
-                        APP.DB.NAME,
-                        APP.DB.VERSION,
-                        widgetTeamMember.getDocumentData(APP.DB.DOCUMENT)
-                    )
-                    .done(loadMembersData)
-                    .progress(notifyProggress);
-            },
-            // load all objects from document into widget
-            loadMembersData = function () {
-                localDB.loadIndexedDBData(APP.DB.DOCUMENT, 'readonly')
-                    .done(function (data) {
-                        var hasRequestData = $.isArray(data) && data.length > 0;
-                        
-                        hasRequestData && widgetTeamMember.refreshTableWidget($tableMembers, data);
-                        $tableMembers.toggleClass('hide', !hasRequestData);
-                    })
-                    .fail(function () {
-                        Materialize.toast('Fail, Cannot connect your Database.', 3000, 'rounded');
-                    })
-                    .always(function () {
-                        $undoModified.trigger('click');
-                    })
-                    .progress(notifyProggress);
-            },
-            // insert new object into document
-            insertMemberData = function (optionsMember) {
-                localDB.insertIndexedDBData(APP.DB.DOCUMENT, 'readwrite', optionsMember)
-                    .done(function () {
-                        loadMembersData();
-                        Materialize.toast('Added new Member.', 3000, 'rounded');
-                    })
-                    .fail(function () {
-                        Materialize.toast('Fail, dni must be unique.', 3000, 'rounded');
-                    })
-                    .progress(notifyProggress);
-            },
-            editMemberData = function (optionsMember) {
-                localDB.updateIndexedDBData(APP.DB.DOCUMENT, 'readwrite', optionsMember)
-                    .done(function () {
-                        loadMembersData();
-                        Materialize.toast('Updated Member.', 3000, 'rounded');
-                    })
-                    .fail(function () {
-                        Materialize.toast('Fail, dni must be unique.', 3000, 'rounded');
-                    })
-                    .progress(notifyProggress);
-            },
-            // load data from one object of document into form widget
-            loadFormMemberData = function (indexObject) {
-                localDB.getIndexedDBData(APP.DB.DOCUMENT, 'readonly', indexObject)
-                    .done(function (dataMember) {
-                        resetFormStatus('wrapper__edit-member', ['wrapper__insert-member']);
-                        formWidget.changeMemberData = dataMember;
-                        widgetTeamMember.fillDataForm(formWidget.$form, dataMember);
-                    })
-                    .fail(function () {
-                        Materialize.toast('Fail, could not get data.', 3000, 'rounded');
-                    })
-                    .progress(notifyProggress);
-            },
-            removeMember = function (indexObject) {
-                localDB.removeIndexedDBData(APP.DB.DOCUMENT, 'readwrite', indexObject)
-                    .done(function () {
-                        loadMembersData();
-                        Materialize.toast('Removed Member.', 3000, 'rounded');
-                    });
-            },
-            // validate form input before insert or update member
-            isCustomValidateForm = function () {
-                formWidget.setCustomValidationItems(null);
-                // Issue Materialize : https://github.com/Dogfalo/materialize/issues/1647
-                $('.lean-overlay').remove();
-                formWidget.changeInputStyleState('.js-control-form:input', true);
-                
-                // validate multiple input states
-                var isValidateForm =    formWidget.nativeValidate() &
-                                        formWidget.checkDni($formMemberDni) &
-                                        formWidget.checkBirthDateUI($formMemberBirthdate);
-                
-                !isValidateForm && formWidget.$modalWidget.openModal() ;
-                return isValidateForm;
-            },
-            // actual form values 
-            getOptionsMember = function () {
-                return {
-                    dni: $formMemberDni.val(),
-                    name: $formMemberName.val(),
-                    charge: $formMemberCharge.find(':selected').text(),
-                    birthdate: $formMemberBirthdate.val()
-                };            
-            },
-            notifyProggress = function (msg, type) {
-                switch(type || '') {
-                    case 'info':
-                        console.info(msg);
-                        break;
-                    case 'error':
-                        console.error(msg);
-                        break;
-                    case 'warn':
-                        console.warn(msg);
-                        break;
-                    default:
-                        console.log(msg);
-                }
-            };
-             
-        widgetTeamMember.initDomElements('.datepicker', '.js-control-team-charge');
-        formWidget.setFormMessages(widgetTeamMember.getMessageValidation());
-        formWidget.$form
-            .on('submit', function (ev) {
-                ev.preventDefault(); // prevent redirect
-            })
-            .on('click', '.js-submit-insert-member', function () {
-                if (isCustomValidateForm()) {
-                    insertMemberData(getOptionsMember());    
-                }
-            })
-            .on('click', '.js-submit-edit-save-member', function () {
-                var optionsMember = $.extend(true, {}, formWidget.changeMemberData, getOptionsMember()); 
-                if (!$.isEmptyObject(formWidget.changeMemberData) && isCustomValidateForm()) {
-                    editMemberData(optionsMember);
-                }
-            })
-            .on('click', '.js-submit-edit-remove-member', function () {
-                formWidget.setConfirmationRemove(formWidget.changeMemberData.name);
-            });
-        formWidget.$form.find(':input').on('invalid', function (ev) {
-            ev.preventDefault(); // prevent show mesage native validity
-        });
-        $undoModified.on('click', function () {
-            formWidget.changeMemberData = null;
-            resetFormStatus('wrapper__insert-member', ['wrapper__edit-member']);
-            formWidget.$modalRemove.closeModal();
-        });
-        $removeMember.on('click', function () {
-            removeMember(formWidget.changeMemberData.primaryKey);
-        });
-        $tableMembers.find('tbody').on('click', 'tr', function () {
-            var indexObject = this.dataset.indexeddbIndex;
-            loadFormMemberData(indexObject);
-        });
-        openMemberDatabase();
-    });
+    
+    $(d).ready($.proxy(APP.initialize, APP));
 }(jQuery, window, document, IndexedDB, FormValidation, Materialize, inserTeamMemberFactory));
